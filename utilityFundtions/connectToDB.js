43bc70c9e2fd877481758b866906c60974cec733
maxpoolpixie <chakri.medcv@gmail.com>
2024-10-10 12:20:04 +0600

    push

--- utilityFundtions/connectToDB.js
+++ utilityFundtions/connectToDB.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose")
 require("dotenv").config();
 
 const connectToDB = () => {
+    console.log(1)
     mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASS}@cluster0.6oyupqe.mongodb.net/personal_finance`, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
